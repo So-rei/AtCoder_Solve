b975169d1e97eaa3ead6d9010f29c1f36713dada
So-rei <so.rei774@gmail.com>
2021-09-27 12:41:09 +0900

    ab220_c AC

--- AtCoderApp/ab220_c.cs
+++ AtCoderApp/ab220_c.cs
@@ -1,16 +1,80 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace AtCoderApp
-//{
-//    //class Program
-//    //{
-//    //    static void Main(string[] args)
-//    //    {
-//    //        new aB220_c();
-//    //    }
-//    //}
+namespace AtCoderApp
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        new aB220_c();
+    //    }
+    //}
+
+    public class aB220_c
+    {
+        public aB220_c()
+        {
+            //input-------------
+            var N = In.Read<int>();
+            var AN = In.ReadAry<long>().ToArray();
+            long X = In.Read<long>();
+
+            //calc----
+            const int Split = 64; //だいたいこのぐらいまで行けば十分なのでは
+            //分割して合計を足して計算量削減する
+
+            //2周目があるかどうかはすぐ分かるので除去
+            long cX = X % AN.Sum();
+            long Cnt = (X / AN.Sum()) * N;
+
+            HalfCalc(AN, cX, Cnt);
+            return;
+
+            //ローカル関数------------------------------------------------------------------
+            void HalfCalc(long[] AList, long cX, long Cnt)
+            {
+                if (AList.Count() < Split) //Split個以下
+                {
+                    LastCalc(AList, cX, Cnt);
+                    return;
+                }
+
+                //Split個以上の場合、分割計算
+                var ANhalf1 = AList.Where((p, index) => index < Math.Ceiling((double)AList.Count() / 2)).ToArray();
+                //var ANhalf1 = AList[0..(int)(AList.Count() / 2 + 1)]; //c# 8.0～
+
+                if (cX - ANhalf1.Sum() < 0)//前半でおわる
+                    HalfCalc(ANhalf1, cX, Cnt);
+                else //後半
+                {
+                    cX -= ANhalf1.Sum();
+                    Cnt += ANhalf1.Count();
+
+                    var ANhalf2 = AList.Where((p, index) => index >= Math.Ceiling((double)AList.Count() / 2)).ToArray();
+                    //var ANhalf2 = AList[(int)(AList.Count() / 2 + 1)..]; //c# 8.0～
+                    HalfCalc(ANhalf2, cX, Cnt);
+                }
+                return;
+            }
+
+            //最後の部分配列、残り数値、今までの回数
+            void LastCalc(long[] AList, long cX, long Cnt)
+            {
+                for (int i = 0; i < AList.Length; i++)
+                {
+                    cX -= AList[i];
+                    if (cX < 0)
+                    {
+                        Out.Write((long)(Cnt + i + 1));
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
 
 //20210926 これだとTLE2発生
 //ちゃんと孫計算まで出来るようにしないと多分ダメ
