626246c6a96f1b260e29d3a148be11661219396c
So-rei <so.rei774@gmail.com>
2021-11-08 13:10:56 +0900

    abc226_d AC*

--- AtCoderApp/abc226.cs
+++ AtCoderApp/abc226.cs
@@ -166,4 +166,44 @@ namespace AtCoderApp
             }
         }
     }
+    public class abc226_d
+    {
+        public abc226_d()
+        {
+            //input-------------
+            var N = In.Read<int>();
+            var xy = new List<int[]>();
+            for (int i = 0; i < N; i++)
+            {
+                var c = In.ReadAry<int>().ToArray();
+                xy.Add(c);
+            }
+
+            //output------------
+            var ptn = new List<string>(); //魔法の組み合わせ
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j) continue;
+                    var cx = xy[i][0] - xy[j][0];
+                    var cy = xy[i][1] - xy[j][1];
+                    int gcd = Math.Abs(Gcd(cx, cy));//最大公約数 ※マイナスは＋にすることに注意
+                    ptn.Add((cx / gcd).ToString() + "," + (cy / gcd).ToString());
+                }
+            }
+            Out.Write(ptn.Distinct().Count());
+        }
+
+        //最大公約数
+        public static int Gcd(int a, int b)
+        {
+            return a > b ? GcdRecursive(a, b) : GcdRecursive(b, a);
+        }
+
+        private static int GcdRecursive(int a, int b)
+        {
+            return b == 0 ? a : GcdRecursive(b, a % b);
+        }
+    }
 }
