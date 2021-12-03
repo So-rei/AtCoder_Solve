a06c5bf7439043b184f0fac433fb694d872da246
So-rei <so.rei774@gmail.com>
2021-12-03 23:37:07 +0900

    基本クラスちょっと手直し

--- AtCoderApp/Program.cs
+++ AtCoderApp/Program.cs
@@ -8,50 +8,23 @@ namespace AtCoderApp
     {
         static void Main(string[] args)
         {
-            new abc230_dx();
+            new xxx();
         }
     }
 
-    public class abc230_dx
+    public class xxx
     {
-        public abc230_dx()
+        public xxx()
         {
             //input-------------
             var ND = In.ReadAry<int>().ToArray();
             var N = ND[0];
             var D = ND[1];
 
-            var LR = new int[N][];
-            for (int i = 0; i < N; i++)
-            {
-                var _LR = In.ReadAry<int>().ToArray();
-                LR[i] = (new int[]{ _LR[0], _LR[1] });
-            }
+            var LR = In.ReadManyAry<int>(N).ToArray();
 
-            //output------------
-            LR = LR.OrderBy(p => p[1]).ToArray();
-
-            int cnt = 0;
-            int right = 0;
-            for (int i = 0; i < N; i++)
-            {
-                if (right < LR[i][0]) //まだ殴られていない壁だったら
-                {
-                    right = LR[i][1] + D - 1; //そこが左端にかかるように殴る
-                    cnt++;
-                }
-            }
-
-            //while (true)
-            //{
-            //    var point = LR.Min(p => p[1]) + D - 1; //一番左にある壁の右端を殴る
-            //    LR = LR.Where(p => p[0] > point).ToArray(); //壊れたやつは削除             
-            //    cnt++;
-            //    if (LR.Count() == 0) break;
-            //}
-
-            Out.Write(cnt);
-            
+            int k = 0;
+            k++;
             //-------------------------------
         }
     }
@@ -64,7 +37,7 @@ static class In
     public static T Read<T>() { var s = Console.ReadLine(); return (T)Convert.ChangeType(s, typeof(T)); }
     public static IEnumerable<T> ReadAry<T>() { return Array.ConvertAll(Console.ReadLine().Split(' '), e => (T)Convert.ChangeType(e, typeof(T))); }
     public static IEnumerable<T> ReadMany<T>(long n) { for (long i = 0; i < n; i++) yield return Read<T>(); }
-    public static IEnumerable<IEnumerable<T>> ReadManyAry<T>(long n) { for (long i = 0; i < n; i++) yield return ReadAry<T>(); }
+    public static IEnumerable<IList<T>> ReadManyAry<T>(long n) { for (long i = 0; i < n; i++) yield return ReadAry<T>().ToArray(); }
 }
 
 static class Out
