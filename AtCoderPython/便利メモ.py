e9c8c4e9ab680cfbbf9afd8f8389f109496932ef
So-rei <so.rei774@gmail.com>
2022-04-19 19:31:15 +0900

    pythonサンプルを更新

--- AtCoderPython/便利メモ.py
+++ AtCoderPython/便利メモ.py
@@ -132,3 +132,28 @@ cLi = sorted(gsb, key = lambda x:(-x[0],-x[1],-x[2]))
 
 
 
+#関数の計算結果のキャッシュを覚えてくれるlru_cache(dp問題などに転用できる)------------------------------------------------
+#実装例-
+(N,M,K) = [int(s) for s in input().rstrip().split(' ')]
+MOD = 998244353 
+
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def calc(n,rest): #残り回数(N→0),残りsum(K-SumA(0-(n-1))
+    if (rest == 0): return 0
+
+    if (n == 1): #最終回
+        return min(rest,M)
+    else:
+        tmp_cnt = 0
+        ptn = min(rest,M)
+        i = 1
+        while (i <= ptn):
+            tmp_cnt += calc(n-1, rest - i) % MOD
+            i += 1
+
+        return tmp_cnt
+
+sumall = calc(N,K) % MOD
+print(sumall)
+#----------------------------------------------------------------------------------------------------------------------
