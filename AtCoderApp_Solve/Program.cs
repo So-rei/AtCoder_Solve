fe0d576ff54610a6a79a34418fc9c3bb3aca17b1
So-rei <so.rei774@gmail.com>
2022-05-08 23:33:47 +0900

    素数関係のアルゴリズムを追加

--- AtCoderApp_Solve/Program.cs
+++ AtCoderApp_Solve/Program.cs
@@ -9,98 +9,16 @@ namespace AtCoderApp
     {
         static void Main(string[] args)
         {
-            new arc139_b();
+            new abc250_d();
         }
 
-        public class arc139_b
+        public class abc250_d
         {
-            public arc139_b()
+            public abc250_d()
             {
                 //input-------------
-                var T = In.Read<int>();
-                var C = new (long N, long A, long B, long X, long Y, long Z)[T];
-                for (int i = 0; i < T; i++)
-                    (C[i].N, C[i].A, C[i].B, C[i].X, C[i].Y, C[i].Z) = In.ReadTuple6<int>();
-
+                var r = Algo.PrimeList(200, -1).ToArray();
                 //output------------
-                for (int i = 0; i < T; i++)
-                {
-                    var Xy = C[i].X * C[i].A <= C[i].Y;
-                    var Xz = C[i].X * C[i].B <= C[i].Z;
-
-
-                    if (Xy || Xz)
-                    {
-                        //Xが最も一番効率いいなら即終了
-                        if (Xy && Xz)
-                        {
-                            Out.Write(C[i].X * C[i].N);
-                            continue;
-                        }
-                        //Y>Z>XまたはZ>Y>Xの場合も即終了
-                        else if (Xy)
-                        {
-                            long cn = (C[i].N - C[i].N % C[i].B) / C[i].B;
-                            Out.Write(cn * C[i].Z + (C[i].N - cn * C[i].B) * C[i].X);
-                            continue;
-                        }
-                        else if (Xz)
-                        {
-                            long cn = (C[i].N - C[i].N % C[i].A) / C[i].A;
-                            Out.Write(cn * C[i].Y + (C[i].N - cn * C[i].A) * C[i].X);
-                            continue;
-                        }
-                    }
-                    
-                    //それ以外の時、一番効率いいやつ詰める
-                    if ((double)C[i].A / (double)C[i].Y > (double)C[i].B / (double)C[i].Z)
-                        //X>Y>Z
-                        Out.Write(SetKn(C[i]));
-                    else
-                    {
-                        //Y>X>Z
-                        long ab = C[i].B;
-                        C[i].B = C[i].A;
-                        C[i].A = ab;
-                        long yz = C[i].Y;
-                        C[i].Y = C[i].Z;
-                        C[i].Z = yz;
-                        Out.Write(SetKn(C[i]));
-                    }
-                }
-
-                //a/y -> b/zの場合---------
-                //ex)N=12のとき、5,5,1,1 より 5,3,3,1の方がいい可能性がある
-                //この処理のため、途中からナップザックする
-                //効率が良い方=a/y
-                long SetKn((long n, long a, long b, long x, long y, long z) C)
-                {
-                    long cnt = Math.Max(0,((C.n - C.n % C.a) / C.a) - Gcd(C.a,C.b) / C.a);
-
-                    var kn = new long[C.n+1];
-                    kn[cnt * C.a] = cnt * C.y;
-
-                    for (var i = cnt * C.a + 1; i <= C.n; i++)
-                    {
-                        var kmin = kn[i - 1] + C.x;
-                        if (i - C.a >= cnt * C.a) kmin = Math.Min(kmin, kn[i - C.a] + C.y);
-                        if (i - C.b >= cnt * C.a) kmin = Math.Min(kmin, kn[i - C.b] + C.z);
-                        kn[i] = kmin;
-                    }
-
-                    return kn[C.n];
-                }
-
-                //gcd
-                static long Gcd(long a, long b)
-                {
-                    return a > b ? GcdRecursive(a, b) : GcdRecursive(b, a);
-                }
-
-                static long GcdRecursive(long a, long b)
-                {
-                    return b == 0 ? a : GcdRecursive(b, a % b);
-                }
             }
         }
 
@@ -267,6 +185,88 @@ namespace AtCoderApp
                 }
 
                 //iを獲らない選択肢は次のループで行われる
+            }
+        }
+
+        //素数かどうかチェックする
+        public static bool IsPrime(long value)
+        {
+            // 1や0、負数は素数ではない
+            if (value < 2)
+                return false;
+
+            // 2と3は素数である
+            if (value == 2 || value == 3)
+                return true;
+
+            // 2以外の偶数であれば素数ではない
+            if (value % 2 == 0)
+                return false;
+
+            // 平方根を切り上げた整数を求める
+            double d = Math.Pow(value, 0.5);
+            long max = (long)Math.Ceiling(d);
+
+            // 奇数だけ平方根を切り上げた整数まで調べる
+            for (long l = 3; l <= max; l += 2)
+            {
+                if (value % l == 0)
+                    return false;
+            }
+            // ループから抜けてしまった場合は素数である
+            return true;
+        }
+
+        //素数リストを出力する
+        //max何個まで、maxいくつまで　の指定可
+        public static IEnumerable<int> PrimeList(int cnt = -1, int max = -1) 
+        {
+            //1000までは高速に出す
+            var s = "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 "+
+                    "101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 "+
+                    "211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 "+
+                    "307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 " +
+                    "401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 " +
+                    "503 509 521 523 541 547 557 563 569 571 577 587 593 599 " +
+                    "601 607 613 617 619 631 641 643 647 653 659 661 673 677 683 691 " +
+                    "701 709 719 727 733 739 743 751 757 761 769 773 787 797 " +
+                    "809 811 821 823 827 829 839 853 857 859 863 877 881 883 887 " +
+                    "907 911 919 929 937 941 947 953 967 971 977 983 991 997";
+
+            foreach (var sp in s.Split(' '))
+            {
+                int prime = Convert.ToInt32(sp);
+                if (max != -1 && prime > max)
+                    yield break;
+                else
+                {
+                    yield return Convert.ToInt32(sp);
+                    cnt--;
+                    if (cnt == 0) yield break;
+                }
+            }
+
+            //1000以上が必要なときは普通に取得
+            if (cnt > 0)
+            {
+                for (int i = 1001; i < int.MaxValue; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        yield return i;
+                        cnt--;
+                    }
+
+                    if (cnt == 0) yield break;
+                }
+            }
+            else
+            {
+                for (int i = 1001; i <= max; i++)
+                {
+                    if (IsPrime(i))
+                        yield return i;
+                }
             }
         }
     }
